35326e1c38a213be6c8fa3fb74ead89def3f4711
small-j1337 <jerryliang.mv@gmail.com>
2026-03-12 15:18:56 -0700

    Adjust Camera

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.x = 0;
+camera.position.y = 0.5;
+camera.position.z = 1;
+camera.rotation.x = -1;
+camera.rotation.y = 0;
 scene.background = new THREE.Color('rgb(0, 0, 0)');
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
@@ -24,7 +29,7 @@ const material = new THREE.MeshBasicMaterial({color: 0x00ff00 });
 const cube = new THREE.Mesh( geometry, material );
 scene.add(cube);
 */
-camera.position.z = 5;
+
 
 
 loader.load('models/Donut.glb', function ( gltf ) {
@@ -39,5 +44,12 @@ loader.load('models/Donut.glb', function ( gltf ) {
 
 function animate( time ) {
   renderer.render( scene, camera );
+  console.log("Camera Position X:", camera.position.x);
+console.log("Camera Position Y:", camera.position.y);
+console.log("Camera Position Z:", camera.position.z);
+ console.log(
+        "Pitch (X):", camera.rotation.x,
+        "Yaw (Y):", camera.rotation.y
+    );
 }
 renderer.setAnimationLoop( animate );
